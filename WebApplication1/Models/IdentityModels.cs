f310c3f06eab319d3b243075e9b1f280220af90e
andrey94pushkar <pwaxxx94@gmail.com>
2017-04-16 14:52:39 +0300

    Enable migration. Add template of SPA

--- WebApplication1/Models/IdentityModels.cs
+++ WebApplication1/Models/IdentityModels.cs
@@ -23,7 +23,7 @@ namespace WebApplication1.Models
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext()
-            : base("DefaultConnection", throwIfV1Schema: false)
+            : base("SPAAuth", throwIfV1Schema: false)
         {
         }
 
