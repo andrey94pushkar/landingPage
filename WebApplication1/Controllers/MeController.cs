8b63f26c0e30f0eae7cb68491396c5fe1808e44f
DESKTOP-GGOBQG7\BiTaMiHa <andrii.petrenko.l@gmail.com>
2017-04-19 20:40:41 +0300

    Test Comit

--- WebApplication1/Controllers/MeController.cs
+++ WebApplication1/Controllers/MeController.cs
@@ -21,6 +21,7 @@ namespace WebApplication1.Controllers
 
         public MeController()
         {
+            /////todo
         }
 
         public MeController(ApplicationUserManager userManager)
